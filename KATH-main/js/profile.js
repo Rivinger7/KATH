8ab970c044cfe21cbe4c74fd35548c5a70b296c4
soyu <satori562003@gmail.com>
2024-02-17 00:36:30 +0700

    3rd Update

--- KATH-main/js/profile.js
+++ KATH-main/js/profile.js
@@ -6,27 +6,59 @@ var profile_content = document.getElementById('profile_content');
 var myOrder_content = document.getElementById('my-order_content');
 var voucher_content = document.getElementById('voucher_content');
 
-profile.addEventListener('click', function() {
+profile.addEventListener('click', function () {
     displayContent(profile, profile_content);
 });
 
-myOrder.addEventListener('click', function() {
+myOrder.addEventListener('click', function () {
     displayContent(myOrder, myOrder_content);
 });
 
-voucher.addEventListener('click', function() {
+voucher.addEventListener('click', function () {
     displayContent(voucher, voucher_content);
 });
 
 function displayContent(button, content) {
     var allButtons = document.querySelectorAll('.button');
     var allContents = document.querySelectorAll('.content');
-    allButtons.forEach(function(element) {
+    allButtons.forEach(function (element) {
         element.classList.remove('active');
     });
-    allContents.forEach(function(element) {
+    allContents.forEach(function (element) {
         element.classList.remove('active');
     });
     button.classList.add('active');
     content.classList.add('active');
+}
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let order_btns = document.querySelectorAll('.history-button');
+    let banner_contents = document.querySelectorAll('.order-history-content');
+
+    order_btns.forEach(function (button, index) {
+        button.addEventListener('click', function () {
+            if (banner_contents[index].classList.contains('open')) {
+                button.classList.remove('open');
+                banner_contents[index].classList.remove('open');
+            } else {
+                displayHistoryOrder(button, banner_contents[index]);
+            }
+        });
+    });
+});
+
+function displayHistoryOrder(button, content) {
+    var allButtons = document.querySelectorAll('.history-button');
+    var allContents = document.querySelectorAll('.order-history-content');
+    
+    allButtons.forEach(function (button) {
+        button.classList.remove('open');
+    });
+    allContents.forEach(function (content) {
+        content.classList.remove('open');
+    });
+    content.classList.add('open');
+    button.classList.add('open');
 }
